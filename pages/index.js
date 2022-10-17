4583eb172413e9c3d8ea4d65f689b0b07fb61514
Abel Pacara <abel.pacara@gmail.com>
2022-10-17 18:52:50 -0400

    basic

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Index = (props) => {
+  return (    
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>     
+        <h1>Home</h1>
+    </div>
+  );
+}
+
+
+export default Index
