5893a88528e3df52cc62eb33712fd79b3b6defa5
Abel Pacara <abel.pacara@gmail.com>
2022-10-17 22:13:34 -0400

    basic cards

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,89 @@ const Index = (props) => {
   return (    
     <div>
       <Head>
-        <title>Home</title>
-      </Head>     
-        <h1>Home</h1>
+        <title>Next.js Project</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/flatly/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
+      </Head>
+    
+      <div className="container-fluid">
+
+        <div>
+          <i className="bi-youtube" style={{ fontSize: 30 }} />          
+          youtube
+        </div>
+
+        <div className="card-deck">
+
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+          <div className="card">
+            <img className="card-img-top" src="https://semantic-ui.com/images/wireframe/image.png" alt="Card image cap" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>              
+              <p className="card-text">
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>      
     </div>
   );
 }
